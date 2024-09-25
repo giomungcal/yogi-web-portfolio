fb6e389c316f5f2934908b8571a8624cefc1d7c8
Gio Mungcal <gcmungcal@gmail.com>
2024-09-25 15:58:36 +0800

    - Fixed capitalization issues on interests.tsx file

--- app/_constants/interests.tsx
+++ app/_constants/interests.tsx
@@ -17,7 +17,6 @@ export const INTERESTS: INTERESTS[] = [
         }}
         src="https://www.youtube.com/embed/4GYHbqslFL4?si=X3TygJ8KdWLlfCeM"
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
