c36dca229f3f26ea8bc5d6250d44fbb9eb5b9b04
Gio Mungcal <gcmungcal@gmail.com>
2024-10-16 01:07:22 +0800

    Changes: - Updated social links in homepage - Updated tech stack with Supabase and FIrebase

--- app/_constants/social_links.ts
+++ app/_constants/social_links.ts
@@ -7,17 +7,17 @@ type SocialLinksItem = {
 export const SOCIAL_LINKS: SocialLinksItem[] = [
   {
     name: "github",
-    link: process.env.GITHUB_URL as string,
+    link: "https://github.com/giomungcal",
     img: "/assets/images/github-square.svg",
   },
   {
     name: "linkedin",
-    link: process.env.LINKEDIN_URL as string,
+    link: "https://www.linkedin.com/in/giomungcal",
     img: "/assets/images/linked-in.svg",
   },
   {
     name: "behance",
-    link: process.env.BEHANCE_URL as string,
+    link: "https://www.behance.net/giomungcal",
     img: "/assets/images/behance.svg",
   },
 ];
